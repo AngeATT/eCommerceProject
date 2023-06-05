fec2ace823078bc369661d1bb3bb782b608392ba
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-06-05 22:45:26 +0000

    ajout des features dans l'application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [StoreModule,
     BrowserModule
   ],
   providers: [],
