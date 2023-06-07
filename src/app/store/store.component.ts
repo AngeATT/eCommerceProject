7e820977196c434dc9000940b2e335af45c7dcbf
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-06-07 23:37:06 +0000

    ajout de la feature panier

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ProductRepository } from "../model/product.repository";
 import { Product } from "../model/product.model";
+import { Cart } from "../model/cart.model";
 
 @Component(
     {
@@ -14,7 +15,7 @@ export class StoreComponent{
     productsPerPage = 4;
     selectedPage = 1;
 
-    constructor(private repository: ProductRepository){}
+    constructor(private repository: ProductRepository, private cart: Cart){}
 
     get products() : Product[]{
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -44,4 +45,7 @@ export class StoreComponent{
     return Math.ceil(this.repository
     .getProducts(this.selectedCategory).length / this.productsPerPage)
     }
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+        }
 }
