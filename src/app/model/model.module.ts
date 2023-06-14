90f8c05a8caae3cdbee694b2a704bd714d5f4632
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-06-14 14:08:11 +0000

    ajout de la fonctionnalités pour passer des ordres

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
+import { Order } from './order/order.model';
+import { OrderRepository } from "./order/order.repository";
 
 @NgModule({
-    providers: [ProductRepository,StaticDataSource,Cart]
+    providers: [ProductRepository,StaticDataSource,Cart,Order,OrderRepository],
+    declarations: [
+      Order
+    ]
 })
 
 export class ModelModule{}
